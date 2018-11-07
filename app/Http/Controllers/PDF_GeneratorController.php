8c336f3f10855e8b4a2c972e18eaaed817c32f0b
lancelotx999 <seanx999@hotmail.com>
2018-11-07 22:39:27 +0800

    Invoice system <WIP>

--- app/Http/Controllers/PDF_GeneratorController.php
+++ app/Http/Controllers/PDF_GeneratorController.php
@@ -30,11 +30,35 @@ class PDF_GeneratorController extends Controller
     }
 
 	public function HTMLtoPDF(){
-		$users = User::all();
-		$contract = Contract::findOrFail('5be2c271339b5754de60b397')->get();
-		$contracts = Contract::all();
+		//provider_id & receiver_id is loaded from POST
+		$provider_id = "5be2c26f339b5754de60b392";
+		$receiver_id = "5be2e673339b57708e1ab802";
 
-		$pdf = PDF::loadView('PDF.receipt',  compact('contracts'));
+		$provider = User::findOrFail($provider_id);
+		$receiver = User::findOrFail($receiver_id);
+
+		$contracts = Contract::where('receiver_id','=', $receiver_id)->where('provider_id','=', $provider_id)->get();
+
+		$total = 0;
+
+		foreach ($contracts as $contract) {
+			$total = $total + $contract->price;
+		}
+
+		$taxed = $total*0.25;
+		$grandTotal = $taxed + $total;
+
+		$data = new class{};
+
+		$data->receiver = $receiver;
+		$data->provider = $provider;
+		$data->total = $total;
+		$data->taxed = $taxed;
+		$data->grandTotal = $grandTotal;
+		$data->contracts = $contracts;
+
+		// return view('PDF.invoice',  compact('data'));
+		$pdf = PDF::loadView('PDF.invoice',  compact('data'));
 
 
 		return $pdf->download('invoice.pdf');
